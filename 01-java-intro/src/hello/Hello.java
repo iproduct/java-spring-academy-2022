bb3e3eabfbe76a5f16f4f55cfea6749fc301c72b
Trayan Iliev <office@iproduct.org>
2022-07-12 21:28:57 +0300

    more output added to demo

--- 01-java-intro/src/hello/Hello.java
+++ 01-java-intro/src/hello/Hello.java
@@ -1,7 +1,11 @@
 package hello;
 
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java World");
+        System.out.println("Hello World");
+        System.out.println("Today is: " + new Date());
+        System.out.println("Do you like Java?");
     }
 }
