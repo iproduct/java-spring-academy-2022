209758e94951fa4a9b4814f8c13d5444ba31e293
Trayan Iliev <office@iproduct.org>
2022-07-12 22:27:25 +0300

    message changed slightly

--- 01-java-intro/src/hello/Hello.java
+++ 01-java-intro/src/hello/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Today is: " + new Date());
-        System.out.println("Do you like Java?");
+        System.out.println("Do you like a Java?");
     }
 }
