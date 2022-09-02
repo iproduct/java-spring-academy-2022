22dff6bcf259d056acab3ca79bae1b891a32bc88
Trayan Iliev <office@iproduct.org>
2022-09-02 21:54:58 +0300

    demos and presentations added

---
+++ 11-spring-core-programmatic/src/main/java/ws/spring/config/ApplicationConfig.java
@@ -0,0 +1,61 @@
+package ws.spring.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.annotation.Order;
+import ws.spring.MainApp;
+import ws.spring.client.impl.ConsoleArticlePresenter;
+import ws.spring.dao.impl.ArticleRepositoryMemoryImpl;
+import ws.spring.model.Article;
+import ws.spring.qualifier.RepositoryProvider;
+import ws.spring.service.ArticleProvider;
+import ws.spring.service.impl.FromPropertyFileArticleProvider;
+import ws.spring.service.impl.RepositoryArticleProvider;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Configuration
+@ComponentScan(basePackageClasses = MainApp.class)
+@PropertySource("classpath:blogs-config.properties")
+public class ApplicationConfig {
+    @Value("${blogs.titles:Default Article}")
+    private String[] titles;
+
+    @Bean(name = "articleRepo")
+    @Scope("singleton")
+    @Order(1)
+    public ArticleRepositoryMemoryImpl articleRepository() {
+        return new ArticleRepositoryMemoryImpl();
+    }
+
+    @Bean(name = "repoProvider", initMethod = "init")
+    @RepositoryProvider
+    public RepositoryArticleProvider repoArticleProvider() {
+        RepositoryArticleProvider provider = new RepositoryArticleProvider();
+        provider.setArticleRepository(articleRepository());
+        return provider;
+    }
+
+//    @Bean(name = "fromPropertyFileProvider")
+//    @RepositoryProvider
+//    @Qualifier("fromPropertyProvider")
+//    public FromPropertyFileArticleProvider fromPropertyFileArticleProvider() {
+//        return new FromPropertyFileArticleProvider();
+//                Arrays.asList(titles).stream().map(
+//                        title -> new Article(System.nanoTime() & (new Random().nextLong()),
+//                                title, title + " content ...")).collect(Collectors.toList()));
+//    }
+
+    @Bean(name = "presenter")
+    public ConsoleArticlePresenter articleRepository(
+            @Qualifier("fromPropertyProvider") ArticleProvider provider) {
+        return new ConsoleArticlePresenter(provider);
+    }
+
+
+
+
+}
