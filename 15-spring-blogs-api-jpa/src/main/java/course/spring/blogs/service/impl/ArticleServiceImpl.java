53bbbc22bb5bd91d0c47bef4ede2d2dcdb332119
Trayan Iliev <office@iproduct.org>
2022-09-21 20:28:59 +0300

    transaction support implemented using programmatic transactions

--- 15-spring-blogs-api-jpa/src/main/java/course/spring/blogs/service/impl/ArticleServiceImpl.java
+++ 15-spring-blogs-api-jpa/src/main/java/course/spring/blogs/service/impl/ArticleServiceImpl.java
@@ -5,26 +5,44 @@ import course.spring.blogs.exception.InvalidEntityDataException;
 import course.spring.blogs.exception.NonexistingEntityException;
 import course.spring.blogs.service.ArticleService;
 import course.spring.blogs.dao.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.transaction.TransactionDefinition.ISOLATION_REPEATABLE_READ;
+import static org.springframework.transaction.annotation.Isolation.REPEATABLE_READ;
 
 /**
  * ArticleService default implementation
  */
 @Service
 @Transactional
+@Slf4j
 public class ArticleServiceImpl implements ArticleService {
     private ArticleRepository articleRepo;
+    //    private TransactionTemplate template;
+    private PlatformTransactionManager transactionManager;
 
     @Autowired
-    public ArticleServiceImpl(ArticleRepository articleRepo) {
+    public ArticleServiceImpl(ArticleRepository articleRepo, TransactionTemplate template, PlatformTransactionManager manager) {
         this.articleRepo = articleRepo;
+//        this.template = template;
+        this.transactionManager = manager;
     }
 
     /**
@@ -63,10 +81,50 @@ public class ArticleServiceImpl implements ArticleService {
         return articleRepo.save(article);
     }
 
+//    @Override
+//    @Transactional(propagation = Propagation.REQUIRED)
+//    public List<Article> createBatch(List<Article> articles) {
+//        return articles.stream().map(this::create).collect(Collectors.toList());
+//    }
+
+//    @Override
+//    public List<Article> createBatch(List<Article> articles) {
+//        return template.execute(new TransactionCallback<List<Article>>() {
+//            @Override
+//            public List<Article> doInTransaction(TransactionStatus status) {
+//                return articles.stream()
+//                        .map(article -> {
+//                            try {
+//                                return create(article);
+//                            } catch (ConstraintViolationException ex) {
+//                                log.error(">>> Constraint violation creating article: " + article.toString(), ex);
+//                                status.setRollbackOnly();
+//                                return null;
+//                            }
+//                        }).collect(Collectors.toList());
+//            }
+//        });
+//    }
+
     @Override
-    @Transactional(propagation = Propagation.REQUIRED)
     public List<Article> createBatch(List<Article> articles) {
-        return articles.stream().map(this::create).collect(Collectors.toList());
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED);
+        def.setTimeout(5);
+        def.setName("createArticlesBatchTransaction");
+        def.setIsolationLevel(ISOLATION_REPEATABLE_READ);
+
+        // Do in transaction
+        List<Article> createdArticles = List.of();
+        var status = transactionManager.getTransaction(def);
+        try {
+            createdArticles = articles.stream().map(this::create).collect(Collectors.toList());
+        } catch (ConstraintViolationException ex) {
+            log.error(">>> Constraint violation creating articles: " + articles.toString(), ex);
+            transactionManager.rollback(status);
+            return createdArticles;
+        }
+        transactionManager.commit(status);
+        return createdArticles;
     }
 
 
